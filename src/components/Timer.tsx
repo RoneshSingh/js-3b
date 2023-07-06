9d9c572cb7d8332e3e36b8ecb1dba7ded0aecf95
RoneshSingh <78497324+RoneshSingh@users.noreply.github.com>
2023-07-06 15:36:33 -0400

    css implemented

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "../assets/timer.css";
 
 function Timer() {
   const [seconds, setSeconds] = useState(0);
@@ -28,11 +29,19 @@ function Timer() {
 
   return (
     <div>
-      <p>"Life is a waste of Time, Time is a waste of life.</p>
-      <p>Why not get wasted all the Time and have the time of our life"</p>
-      <h1>Timer: {seconds} seconds</h1>
-      <button onClick={handleStart}>Start</button>
-      <button onClick={handleStop}>Stop</button>
+      <p className="pTag">"Life is a waste of Time, Time is a waste of life.</p>
+      <p className="pTag">
+        Why not get wasted all the Time and have the time of our life"
+      </p>
+      <h1 className="h1">Timer: {seconds} seconds</h1>
+      <div className="div">
+        <button className="button" onClick={handleStart}>
+          Start
+        </button>
+        <button className="button" onClick={handleStop}>
+          Stop
+        </button>
+      </div>
     </div>
   );
 }
